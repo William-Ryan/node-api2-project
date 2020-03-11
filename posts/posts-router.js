983b64c7b779e3f80af7a1c557110edf37bffadc
William <billy.ryan.rom@gmail.com>
2020-03-11 10:41:40 -0400

    Add Comment Post Request

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -4,4 +4,30 @@ const Posts = require("../data/db.js")
 
 const router = express.Router();
 
+router.post('/', (req, res) => {
+    const data = req.body;
+    if(!data.title || !data.contents) {
+        res.status(400).json({
+            errorMessage: "Please provide title and contents for the post."
+        })
+    } else if (data.title && data.contents) {
+    Posts
+        .insert(req.body)
+        .then(post => {
+            res.status(201).json(post);
+        })
+        .catch(err => {
+            console.log("Error inserting post to db", err);
+            res.status(500).json({
+                message: "error inserting the post"
+            })
+
+    })
+    } else {
+        res.status(500).json({
+            message: "Error inserting the post"
+        })
+    }
+});
+
 module.exports = router
